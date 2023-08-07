96751a77eee7af82c8364799129b0d3892ffa467
RoudroMehedi <roudro.m@gmail.com>
2023-08-07 15:30:00 +0200

    Bg image changed

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
     <div
       class="w-1/2"
       style="
-        background-image: url(https://images.unsplash.com/photo-1690555575753-7aa27df96b52?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80);
+        background-image: url(https://images.unsplash.com/photo-1682687220742-aba13b6e50ba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80);
         background-repeat: no-repeat;
         background-size: cover;
       "
     >
-      <h1 class="mb-5 text-2xl mt-10 ml-10 text-white">Kosmos!</h1>
+      <h1 class="mb-5 text-2xl mt-10 ml-10 text-white">Hi There!</h1>
     </div>
     <div class="w-1/2 flex flex-col justify-center items-center bg-gray-200">
       <template v-if="!loggedIn">
